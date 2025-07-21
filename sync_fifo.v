02abc64caa4ee419a271204cdfe419cbdbabe722
Varun Pateliya <varunpateliya1@gmail.com>
2025-07-21 15:39:53 +0530

    Initial commit

---
+++ sync_fifo.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.07.2025 15:23:56
+// Design Name: 
+// Module Name: sync_fifo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sync_fifo(
+
+    );
+endmodule
